1d6abc7188cca1454af02bc93e17a23e2fd3cffe
Desant <anturated@gmail.com>
2026-04-26 18:23:25 +0200

    feat: add options after end

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -135,6 +135,29 @@ in
         echo "> Attach:"
         echo "    ''${G}docker attach minecraft-${name}''${N}"
         echo "• ──────────────────────────────────────────────── •"
+
+        echo -ne "> What's next? (l)ogs / (a)ttach / (N)o action"
+
+
+        read -n 1 -r choice
+        echo  # move to next line after keypress
+
+        case "$choice" in
+          l|L)
+            echo "> Opening logs for ${name}..."
+            echo "• ──────────────────────────────────────────────── •"
+            docker compose -f "$DEST/compose.yaml" logs -f
+            ;;
+          a|A)
+            echo "> Attaching to ${name}..."
+            echo "• ──────────────────────────────────────────────── •"
+            docker attach "minecraft-${name}"
+            ;;
+          *)
+            echo "> Goodbye."
+            echo "• ──────────────────────────────────────────────── •"
+            ;;
+        esac
       '';
     }
     + "/bin/deploy-${name}"
